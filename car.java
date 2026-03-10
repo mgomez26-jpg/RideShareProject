f16fbaaf2e908b014216ab9bc92151bccd4a818e
Mariana Gomez <mgomez26@students.polytechnic.org>
2026-03-10 10:11:58 -0700

    Add fields to Car class for rideshare simulation

--- car.java
+++ car.java
@@ -1,3 +1,11 @@
 public class car {
-    
+    //fields
+    private ArrayList<Passenger> people;
+    private int destination;
+    private int initialStation;
+    private boolean forward;
+    private int currentStation;
+    private boolean moveable;
+    private int revenue;
+    private int distanceTraveled;
 }
