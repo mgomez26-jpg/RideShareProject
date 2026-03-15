32cb35d4548d605659f45b5ad90912867c9b7429
Mariana Gomez <mgomez26@students.polytechnic.org>
2026-03-15 11:35:49 -0700

    Initialize Road class with core fields

--- Road.java
+++ Road.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Road {
+    private int miles;
+    private int revenue;
+    private ArrayList<Station> stations;
+    private ArrayList<Car> cars;
+    private Random.random;
     
 }
