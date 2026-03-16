6df185991bf72c3e7f5ff386e71ce705f69d8ec7
Mariana Gomez <mgomez26@students.polytechnic.org>
2026-03-15 19:59:25 -0700

    Created Station class with fields for station ID and passenger list. Added constructor to initialize the station and its passenger collection.

--- Station.java
+++ Station.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public class Station {
     
-}
+private int id;
+private ArrayList<Passenger> passengers;
+
+public Station(int id;
+    public Station(int id) {
+        this.id = id;
+        passengers = new ArrayList<>();
+    }
+
+)
